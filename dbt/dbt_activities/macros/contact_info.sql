de722c5293c22c298d776c3758ea4b8e6a071061
Anna Knyazeva <aknyazeva22@gmail.com>
2025-06-30 12:01:56 +0200

    start dbt project

---
+++ dbt/dbt_activities/macros/contact_info.sql
@@ -0,0 +1,12 @@
+{% macro test_contact_info(model) %}
+SELECT *
+FROM {{ ref('stg_raw_degustation_data') }}
+WHERE address_line1 IS NULL
+  AND address_line2 IS NULL
+  AND address_line3 IS NULL
+  AND address_line4 IS NULL
+  AND modile_phone_number IS NULL
+  AND landline_number IS NULL
+  AND email IS NULL
+  AND website IS NULL
+{% endmacro %}
