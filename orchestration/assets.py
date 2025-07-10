981c14710a6a230320015bcd341fd17edde2f1a4
Anna Knyazeva <aknyazeva22@gmail.com>
2025-07-10 09:43:09 +0200

    mark compute_kind for assets

--- orchestration/assets.py
+++ orchestration/assets.py
@@ -4,32 +4,34 @@ from dagster import AssetExecutionContext, asset
 from dagster_dbt import dbt_assets
 
 # Infrastructure
-@asset
+@asset(compute_kind="python")
 def azure_psql_server(context: AssetExecutionContext) -> None:
     context.log.info("Create infrastructure with terraform")
     runpy.run_module("scripts.create_postgresql_server", run_name="__main__")
 
 
 # Extract
-@asset
+@asset(compute_kind="python")
 def raw_data(context: AssetExecutionContext) -> None:
     context.log.info("Running extract step")
     runpy.run_module("scripts.download_raw_data", run_name="__main__")
 
 # Load
 @asset(
-    deps=[raw_data, azure_psql_server, dbt_profiles],
-    description="Load degustations.csv into PostgreSQL as a raw table."
-)
-def raw_degustation_data(context: AssetExecutionContext) -> None:
-    context.log.info("Running load step")
-    runpy.run_module("scripts.load_raw_data", run_name="__main__")
-
-# Load
-@asset(
     deps=[azure_psql_server],
-    description="Create dbt profiles from infra creation step."
+    description="Create dbt profiles from infra creation step.",
+    compute_kind="python",
 )
 def dbt_profiles(context: AssetExecutionContext) -> None:
     context.log.info("Creating profiles for dbt from the infrastructure")
     runpy.run_module("scripts.generate_profiles", run_name="__main__")
+
+# Load
+@asset(
+    deps=[raw_data, azure_psql_server, dbt_profiles],
+    description="Load degustations.csv into PostgreSQL as a raw table.",
+    compute_kind="python",
+)
+def raw_degustation_data(context: AssetExecutionContext) -> None:
+    context.log.info("Running load step")
+    runpy.run_module("scripts.load_raw_data", run_name="__main__")
