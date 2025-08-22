66d26f3c5aef1fb31fb20ff0fb46922a46f429da
Anna Knyazeva <aknyazeva22@gmail.com>
2025-08-22 15:49:27 +0200

    sql schema as parameter

--- scripts/load_raw_data.py
+++ scripts/load_raw_data.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from typing import Optional
 import pandas as pd
 from sqlalchemy import create_engine, inspect
@@ -11,7 +12,6 @@ from utils import determine_terraform_dir, load_terraform_outputs
 
 CSV_PATH = 'data/degustations.csv'
 TABLE_NAME = 'raw_degustation_data'
-SCHEMA = 'public'
 
 load_dotenv()
 # Load environment variables
@@ -21,6 +21,7 @@ DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = "localhost" if DB_PROVIDER == "local" else None
 DB_PORT = os.environ.get('DB_PORT', '5432')
 DB_NAME = os.environ.get('DB_NAME')
+SCHEMA = os.environ.get('DB_SCHEMA', 'public')
 
 def get_engine() -> Engine:
     """
@@ -35,10 +36,10 @@ def get_engine() -> Engine:
             connection_string = outputs["postgres_example_conn_str"]["value"]
         except KeyError as missing:
             sys.exit(f"[fatal] missing key in terraform outputs: {missing}")
-    elif db_provider == "local":
+    elif DB_PROVIDER == "local":
         connection_string = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     else:
-        sys.exit(f"[fatal] provider type is not supported: {db_provider}")
+        sys.exit(f"[fatal] provider type is not supported: {DB_PROVIDER}")
 
     return create_engine(connection_string)
 
